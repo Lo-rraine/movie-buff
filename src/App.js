197ac62ede3f5d6169f3e98710e3d412aeafdff3
Lorrainem <moyolorraine98@gmail.com>
2023-05-23 23:45:26 +0200

    remove fave movie functionality

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import MovieItem from './components/MovieItem';
 import Heading from './components/Heading';
 import SearchInput from './components/SearchInput';
 import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourite';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -22,33 +23,64 @@ const App = () => {
     }
   };
 
-  const addFavouriteMovie = (movie) => {
-		const newFavouriteList = [...favourites, movie];
-		setFavourites(newFavouriteList);
-	};
-
   useEffect(() => { getMovieDetails(searchInputValue); }, [searchInputValue]);
+  useEffect(() => {
+    const storedMovieFaves = JSON.parse(localStorage.getItem('movie-buff-faves'));
+    setFavourites(storedMovieFaves);
+  }, [])
 
-  return (
-    <div className='container-fluid movie-listing'>
 
-      <div className='row d-flex align-items-center mt-4 mb-4'>
-        <Heading heading='Your Movies' />
-        <SearchInput 
-        searchInputValue={searchInputValue} 
-        setSearchInputValue={setSearchInputValue} 
-        />
-      </div>
-
-      <div className='row'>
-        <MovieItem 
-          movies={movies} 
-          favouriteComponent={AddFavourites}
-          handleFavouritesClick={addFavouriteMovie}
-        />
-      </div>
-    </div>
-  );
+const persistenceStorage = (items) => {
+  localStorage.setItem('movie-buff-faves', JSON.stringify(items));
 };
+
+const addFavouriteMovie = (movie) => {
+  const newFavouriteList = [...favourites, movie];
+  setFavourites(newFavouriteList);
+  persistenceStorage(newFavouriteList);
+};
+
+const removeFavouriteMovie = (movie) => {
+  const newFavouriteList = favourites.filter(
+    (favourite) => favourite.imdbID !== movie.imdbID);
+
+  setFavourites(newFavouriteList);
+  persistenceStorage(newFavouriteList);
+};
+
+
+return (
+  <div className='container-fluid'>
+
+    <div className='row d-flex align-items-center mt-4 mb-4'>
+      <Heading heading='Your Movies' />
+      <SearchInput
+        searchInputValue={searchInputValue}
+        setSearchInputValue={setSearchInputValue}
+      />
+    </div>
+
+    <div className='container-fluid movie-listing'>
+      <MovieItem
+        movies={movies}
+        favouriteComponent={AddFavourites}
+        handleFavouritesClick={addFavouriteMovie}
+      />
+    </div>
+
+    <div className='row d-flex align-items-center mt-4 mb-4'>
+      <Heading heading='Favourites' />
+    </div>
+    <div className='container-fluid movie-listing'>
+      <MovieItem
+        movies={favourites}
+        favouriteComponent={RemoveFavourites}
+        handleFavouritesClick={removeFavouriteMovie}
+      />
+    </div>
+  </div>
+);
+};
+
 
 export default App;
