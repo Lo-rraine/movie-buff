4465e2f2266d3eb5d1b170b1fb4102d0d33247c7
Lorrainem <moyolorraine98@gmail.com>
2023-05-31 20:59:10 +0200

    Dev Section

---
+++ src/components/Dev/Dev.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dev = () => {
+  return (
+    <div>
+      <h1>resources: https://dribbble.com/shots/5660843-Movie-card</h1>
+    </div>
+  );
+};
+
+export default Dev;
