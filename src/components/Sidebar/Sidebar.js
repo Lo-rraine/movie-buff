8b62f0079a1a6e34ffd6f1d61d378fd428b94d27
Lorrainem <moyolorraine98@gmail.com>
2023-06-01 22:29:34 +0200

    Build fixes

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -6,7 +6,7 @@ import { Link, useLocation } from "react-router-dom";
 
 function Sidebar() {
   const location = useLocation();
-  const [setSelectedLink] = useState("/");
+  const [SelectedLink, setSelectedLink] = useState("/");
 
   // Set the selected link based on the current location
   useState(() => {
@@ -23,7 +23,7 @@ function Sidebar() {
         {SidebarData.map((val, key) => {
           return (
             <li
-              id={window.location.pathname === val.link ? "active" : ""}
+              id={SelectedLink === val.link ? "active" : ""}
               className="row"
               key={key}
             >
