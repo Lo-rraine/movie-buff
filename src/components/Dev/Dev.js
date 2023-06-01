fc9a1791057dbd0e14df31a05dcc40b8c01da93c
Lorrainem <moyolorraine98@gmail.com>
2023-06-01 22:03:58 +0200

    Dev section

--- src/components/Dev/Dev.js
+++ src/components/Dev/Dev.js
@@ -1,27 +1,12 @@
 import React from "react";
 import "./Dev.css";
-import theDev from "./theDev.jpg";
+import DevBio from "./DevBio.png";
 
 const Dev = () => {
   return (
     <div className="dev-main">
       <div className="dev-inner">
-        <p>
-          Introducing <strong>Lorraine</strong>,
-        </p>
-        <img className="dev-image" src={theDev} alt="Lorraineer" />
-        <div className="dev-text">
-          <p>
-            The friendly neighborhood developer who's constantly caught in a
-            whirlwind of JavaScript, C#, and a whole bunch of APIs
-          </p>
-          <p>
-            With a knack for breaking and building applications, she dances
-            through the coding universe. This is her digital escapades, where
-            laughter, curiosity, and the occasional bug fix collide.
-          </p>
-        </div>
-        <h1>resources: https://dribbble.com/shots/5660843-Movie-card</h1>
+        <img className="dev-image" src={DevBio} alt="Lorraineer" />
       </div>
     </div>
   );
